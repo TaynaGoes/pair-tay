9c179d27708b7691475cb12c8b42f86b1921eedc
StefanyCandida <stefany.candida@gmail.com>
2022-10-24 16:42:12 -0300

    inicio de projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Componentes/Header";
+import Main from "./Componentes/Main"
+import Footer from './Componentes/Footer'
+
+function App() {
+  return (
+    <div>
+       <Header/>
+       <Main />
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
